2db8d9c987a5551ed036a01d159e72f4f4788cb7
Luan <engluanreis@gmail.com>
2023-02-08 12:26:56 +0000

    Navbar estruturada

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {Link} from "react-router-dom"
+
+import "./styles.css";
+export default function Navbar () {
+   return (
+
+        <nav className="navbar">
+        <div>
+        
+            <Link className="logo" to= "/">
+        <h3>All Services</h3>
+        </Link>
+        </div>
+
+        <div>
+           
+            <Link className="nav-link" to="/">
+                Home
+            </Link>
+            <Link className="nav-link" to="/post">
+                Suporte
+            </Link>
+            <Link className="nav-link" to="/contact">
+                Anuncie aqui
+            </Link>
+            <Link className="nav-link" to="/login">
+                Login
+            </Link>
+    
+        </div>    
+        </nav>
+    )
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,15 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom"
 
+import Home from "../pages/home"
+import Navbar from "../components/navbar";
 
 export default function AplicationRoutes () {
-
-    return(
+  return(
         <BrowserRouter>
+        <Navbar />
         <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/post" element={<h1>Post</h1>} />
-
         </Routes>
         </BrowserRouter>
     )
